2d6bc60c61ec1cb5cd1c068fb2b7ea37bb27c1b7
David Maillard <david@dmaillard.com>
2018-04-12 07:50:51 -0300

    Kārbowanec developers

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2014-2018, The Forknote project
-// Copyright (c) 2016-2018, The Karbowanec developers
+// Copyright (c) 2016-2018, The Kārbowanec developers
 //
 // This file is part of Bytecoin.
 //
